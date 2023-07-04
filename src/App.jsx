1bb66d11bf3b603f78591355a0a8892751fca1e1
PabloLimaDelgado <limapablomdz@gmail.com>
2023-07-04 17:21:12 -0300

    V1-REACT

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import {header1} from './components/Header1/header';
+
+function App() {
+  return (
+    header1
+  );
+}
+
+export default App; 
---
+++ src/components/Header1/header.jsx
@@ -0,0 +1,11 @@
+import './header.scss'
+
+
+
+export const header1 = () => {
+    return (
+      <header>
+        <h1>Cheval Marche</h1>
+      </header>
+    );
+  };
